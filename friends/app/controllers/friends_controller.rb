3dfb84ec90054d4923ba8271d666610f4f00fd4a
Rabia Asif <16907370+rabiaasif@users.noreply.github.com>
2022-02-21 01:39:19 -0500

    following tut

--- friends/app/controllers/friends_controller.rb
+++ friends/app/controllers/friends_controller.rb
@@ -25,7 +25,7 @@ class FriendsController < ApplicationController
 
     respond_to do |format|
       if @friend.save
-        format.html { redirect_to friend_url(@friend), notice: "Friend was successfully created." }
+        format.html { redirect_to friend_url(@friend) }
         format.json { render :show, status: :created, location: @friend }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class FriendsController < ApplicationController
   def update
     respond_to do |format|
       if @friend.update(friend_params)
-        format.html { redirect_to friend_url(@friend), notice: "Friend was successfully updated." }
+        format.html { redirect_to friend_url(@friend) }
         format.json { render :show, status: :ok, location: @friend }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class FriendsController < ApplicationController
     @friend.destroy
 
     respond_to do |format|
-      format.html { redirect_to friends_url, notice: "Friend was successfully destroyed." }
+      format.html { redirect_to friends_url, notice: "Friend was successfully destroyed. (keeping this one here as an example)" }
       format.json { head :no_content }
     end
   end
