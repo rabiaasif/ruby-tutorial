0c8c953eee4f6c5989a959dd77b9e5e112277f5e
Rabia Asif <16907370+rabiaasif@users.noreply.github.com>
2022-02-25 00:13:20 -0500

    add some auth

--- friends/app/controllers/friends_controller.rb
+++ friends/app/controllers/friends_controller.rb
@@ -3,6 +3,8 @@ class FriendsController < ApplicationController
   # if a user isnt not authenticated only show index and show page
   before_action :authenticate_user!, except: [:index, :show]
 
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   # GET /friends or /friends.json
   def index
     @friends = Friend.all
@@ -14,7 +16,7 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -23,7 +25,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    # @friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -58,6 +61,11 @@ class FriendsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, notice: "Not allowed to edit this friend" if @friend.nil?
+  end 
 
   private
     # Use callbacks to share common setup or constraints between actions.
