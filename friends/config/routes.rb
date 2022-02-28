014c28247a7bc3c04aa4cdea5dff62581a6a1fe5
Rabia Asif <16907370+rabiaasif@users.noreply.github.com>
2022-02-27 23:25:08 -0500

    follwing tutorial

--- friends/config/routes.rb
+++ friends/config/routes.rb
@@ -5,5 +5,5 @@
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "friends#index"
 end
